e8087baead17b7e9e86f944facbf1a0487e773df
angryn3rd <angrynerd09@gmail.com>
2018-07-29 10:44:25 +0530

    changes were made to the jconnect.php file

--- jconnect.php
+++ jconnect.php
@@ -1,5 +1,5 @@
 <?php
-    $connect=mysql_connect("localhost","root","SsXx*9710");  //this file connects to the database
+    $connect=mysql_connect("localhost","root","");  //this file connects to the database
     if(!$connect)
     {
         echo"Database not connected.";
